643cce1eaa6ca66ab7366f18282950cf08dd6905
Paikan Adham <paikanadham@gmail.com>
2025-10-30 18:55:44 +0500

    Updated for Vercel deployment

--- server.js
+++ server.js
@@ -159,5 +159,4 @@ app.post('/api/quizzes', async (req,res) => {
 app.use(express.static(path.join(__dirname,'public')));
 app.get('*', (req,res)=> res.sendFile(path.join(__dirname,'public','index.html')));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=> console.log(`Server running on http://localhost:${PORT}`));
+module.exports = app;
